220d1c2a07e43770e318aaae1926c81e66195d82
maheshwaram <pradeepmaheshwaram8008@gmail.com>
2025-10-26 11:44:47 +0530

    Ive completed the assignment successfully!!

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
+        System.out.println(p.product(44,90));
+        System.out.println(p.product(23,3445,67));
+        System.out.println(p.product(23.3,34.7));
+
+    }
 
 }
---
+++ src/main/java/com/driver/Product.java
@@ -0,0 +1,13 @@
+package com.driver;
+
+public class Product {
+    public int product(int x,int y,int z){
+        return x*y*z;
+    }
+    public int product(int x,int y){
+        return x*y;
+    }
+    public double product(double x,double y){
+        return x*y;
+    }
+}
